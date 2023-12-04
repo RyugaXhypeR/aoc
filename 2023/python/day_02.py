d3b5b0d884be7018b0ba554be3ec97998e738976
RyugaXhypeR <ryugashyper@gmail.com>
2023-12-05 01:21:15 +0530

    change: code clean up and type hints!

--- 2023/python/day_02.py
+++ 2023/python/day_02.py
@@ -1,67 +1,52 @@
-with open("../inputs/day_02.txt") as file:
-    IN = file.read().strip().splitlines()
+from collections import defaultdict
+from math import prod
 
-TEST1 = """\
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+TEST_INPUT: list[
+    str
+] = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """.splitlines()
 
-R, G, B = 12, 13, 14
+is_final: bool = True
 
 
-def is_valid(ln):
-    for st in ln.split(";"):
-        r, g, b = 0, 0, 0
-        for s in st.split(","):
-            i, c = s.split()
-            if c[0] == "b":
-                b += int(i)
-            elif c[0] == "g":
-                g += int(i)
-            elif c[0] == "r":
-                r += int(i)
-        if r > R or g > G or b > B:
-            return False
-    return True
+def clean_input(aoc_input: list[str]) -> list[dict[str, list[int]]]:
+    games = []
+    for line in aoc_input:
+        color_map: dict[str, list[int]] = defaultdict(list)
+
+        for game in line.split(":")[1].split(";"):
+            for num_balls, color in map(str.split, game.split(", ")):
+                color_map[color].append(int(num_balls))
+        games.append(color_map)
+    return games
 
 
-def max_clr(ln):
-    r, g, b = [], [], []
-    for st in ln.split(";"):
-        for s in st.split(","):
-            i, c = s.split()
-            if c[0] == "b":
-                b += [int(i)]
-            elif c[0] == "g":
-                g += [int(i)]
-            elif c[0] == "r":
-                r += [int(i)]
-    return max(r), max(g), max(b)
+def part1(aoc_input: list[dict[str, list[int]]]) -> int:
+    return sum(
+        i
+        for i, game in enumerate(aoc_input, start=1)
+        if all(map((12).__ge__, game["red"]))
+        and all(map((13).__ge__, game["green"]))
+        and all(map((14).__ge__, game["blue"]))
+    )
 
 
-def part1(inp):
-    s = 0
-    for ln in inp:
-        g, st = ln.split(":")
-        if is_valid(st):
-            s += int(g.split()[-1])
-    return s
+def part2(aoc_input: list[dict[str, list[int]]]) -> int:
+    return sum(prod(map(max, game.values())) for game in aoc_input)
 
 
-def part2(inp):
-    # s = 0
-    # for ln in inp:
-    #     g, st = ln.split(":")
-    #     r, g, b = max_clr(st)
-    #     s += r * g * b
-    # return s
-    regex = re.compile(r"[\s;]")
-    
+def main() -> None:
+    with open("../inputs/day_02.txt") as file:
+        aoc_raw_input = file.read().strip().splitlines()
+
+    aoc_input = clean_input(aoc_raw_input if is_final else TEST_INPUT)
+    print(part1(aoc_input))
+    print(part2(aoc_input))
 
 
 if __name__ == "__main__":
-    print(part1(IN))
-    print(part2(IN))
+    main()
