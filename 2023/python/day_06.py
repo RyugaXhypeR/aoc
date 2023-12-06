b2ac35696d8ebdb6c039236c08ee32df83bc5864
RyugaXhypeR <ryugashyper@gmail.com>
2023-12-06 20:22:20 +0530

    change: using math formula to solve the problem

--- 2023/python/day_06.py
+++ 2023/python/day_06.py
@@ -1,3 +1,4 @@
+from itertools import takewhile
 from math import prod
 
 TEST_INPUT = """\
@@ -12,9 +13,25 @@ def clean_input(aoc_input: str) -> list[list[str]]:
     return [line.split(":")[1].split() for line in aoc_input.splitlines()]
 
 
+def num_winning_runs(time: int, distance: int) -> int:
+    # Brute Force::
+    #   Slight optimization, iterating over [time // 2, time).
+    #   Itertate until the run ((time - i) * i) is less than `distance`,
+    #   then multiply by two (because multiplication is commutative) and
+    #   add 1 if time is even (~time & 1).
+    #
+    #   return len(
+    #       list(takewhile(lambda i: (time - i) * i >
+    #            distance, range(time // 2 + 1, time)))
+    #   ) * 2 + (~time & 1)
+
+    _d = (time**2 - 4 * distance) ** 0.5
+    return int((time + _d) // 2 - (time - _d) // 2)
+
+
 def part1(aoc_input: list[list[str]]) -> int:
     return prod(
-        sum((time - i) * i > distance for i in range(time + 1))
+        num_winning_runs(time, distance)
         for time, distance in map(lambda x, y: (int(x), int(y)), *aoc_input)
     )
 
@@ -22,7 +39,7 @@ def part1(aoc_input: list[list[str]]) -> int:
 def part2(aoc_input: list[list[str]]) -> int:
     # Brute force, executes in ~5s
     time, distance = map(int, map("".join, aoc_input))
-    return sum((time - i) * i > distance for i in range(time + 1))
+    return num_winning_runs(time, distance)
 
 
 def main() -> None:
