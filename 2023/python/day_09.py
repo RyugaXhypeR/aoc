b011dc5acb539aef8d3349e4d236626b346cdacc
RyugaXhypeR <ryugashyper@gmail.com>
2023-12-09 18:55:00 +0530

    `py-day-09`: clean up

--- 2023/python/day_09.py
+++ 2023/python/day_09.py
@@ -1,4 +1,5 @@
-from functools import reduce
+from itertools import pairwise
+from typing import Iterable
 
 TEST_INPUT = """\
 0 3 6 9 12 15
@@ -13,21 +14,19 @@ def parse_input(aoc_raw_input: str) -> list[list[int]]:
     return [[*map(int, line.split())] for line in aoc_raw_input.splitlines()]
 
 
-def to_zero_and_not_beyond(history: list[int], pos: int = -1) -> list[int]:
-    predicted = history[pos]
-    while any(history):
-        history = [history[i + 1] - history[i]
-                   for i in range(0, len(history) - 1)]
-        predicted += history[pos]
-    return predicted
+def to_zero_and_not_beyond(history: list[int]) -> list[int]:
+    return history[-1] + sum(
+        (history := [y - x for x, y in pairwise(history)])[-1]
+        for _ in iter(lambda: any(history), False)
+    )
 
 
-def part1(aoc_input):
+def part1(aoc_input: Iterable[list[int]]) -> int:
     return sum(map(to_zero_and_not_beyond, aoc_input))
 
 
-def part2(aoc_input):
-    return part1([num[::-1] for num in aoc_input])
+def part2(aoc_input: Iterable[list[int]]) -> int:
+    return part1(num[::-1] for num in aoc_input)
 
 
 def main() -> None:
